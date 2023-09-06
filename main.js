a304e9016ad0d1d9f3dc0e4517177e5a2cdbb409
deivymorales <srzorro@gmail.com>
2023-09-05 21:15:23 -0500

    :sparkles: Creando la funcionalidad de crear tarea

---
+++ main.js
@@ -0,0 +1,53 @@
+// Apuntando a los elementos del DOM
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textarea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+
+//Anyadiendo funcionalidad al boton
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+// Validando el formulario.
+let formValidation = () => {
+  if(textInput.value === "") {
+    console.log("failure");
+    msg.innerHTML = "La tarea no puede estar en blanco."
+  }else{
+    console.log("Success");
+    msg.innerHTML = "";
+    acceptData();
+  }
+};
+
+// Almacenar los datos.
+let data = {};
+
+let acceptData = () => {
+  data["text"] = textInput.value;
+  data["date"] = dateInput.value;
+  data["description"] = textarea.value;
+  createTask();
+};
+
+// Creando las tareas
+
+let createTask = () => {
+  tasks.innerHTML += `
+  <div>
+    <span class="fw-bold">${data.text}</span>
+    <span class="small text-secondary">
+      ${data.date}
+    </span>
+    <p>${data.description}</p>
+    <span class="options">
+      <i class="fas fa-edit"></i>
+      <i class="fas fa-trash-alt"></i>
+    </span>
+  </div>
+  `;
+}
